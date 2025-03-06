1e16739ac3e2bc1fd467d0f76c9a29010c1b0014
justin-chu-ics4u-03-25 <justin.chu4@student.tdsb.on.ca>
2025-03-06 07:01:12 +0000

    My answers

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,42 @@
+import java.io.IOError;
+import java.io.IOException;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
 
+	public static boolean validIndex(int[] intarray, int integer) {
+		try {
+			int value = intarray[integer];
+			return true;
+		}
+
+		catch(ArrayIndexOutOfBoundsException error) {
+			return false;
+		}
+	}
+
+	public static int divide(int a, int b) {
+		try {
+			return a / b;
+		}
+
+		catch(ArithmeticException error) {
+			return 0;
+		}
+	}
+
+	public static int safeConvertStringtoInt(String string) {
+		try {
+			return Integer.parseInt(string);
+		}
+
+		catch(NumberFormatException error) {
+			return 0;
+		}
+	}
 }
+
